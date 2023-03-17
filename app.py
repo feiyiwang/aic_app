408f3d16aa553b7677ca89d2842ea97ff117113c
feiyiwang <rephycca@gmail.com>
2023-03-17 12:18:29 +0200

    try

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ ptData = noteData.keys()
 
 @app.route('/')
 def root():
-    return app.send_static_file('individual.html')
+    return app.send_static_file('index.html')
 
 
 @app.route("/getPatientList", methods=["GET"])
